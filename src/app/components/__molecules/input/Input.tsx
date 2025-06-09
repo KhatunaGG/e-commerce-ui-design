fd0d97035683038b774c3578a3308b8f594f41c3
Gagloshvili <KGagloshvili@oriflame.com>
2025-06-09 14:06:30 +0400

    added: sign-up logic

--- src/app/components/__molecules/input/Input.tsx
+++ src/app/components/__molecules/input/Input.tsx
@@ -14,12 +14,10 @@ export type InputPropsType<T extends FieldValues> = {
 };
 
 export default function Input<T extends FieldValues>({
-//   register,
-//   errors,
+  register,
+  // errors,
   fieldName,
 }: InputPropsType<T>) {
-
-    // console.log(register, errors)
   const labelText =
     fieldName === "yourName"
       ? "Your Name"
@@ -39,17 +37,17 @@ export default function Input<T extends FieldValues>({
       >
         {labelText}
       </label>
-      <div className="w-full relative">
+      <div className="w-full relative bg-green-200 rounded-lg">
         <input
+          {...register(fieldName)}
           type="text"
-          className="w-full border-b border-b-[#E8ECEF] outline-none py-1 text-sm "
+          className="w-full border-b border-b-[#E8ECEF] outline-none py-1 px-2 text-sm "
         />
         {fieldName === "password" && (
           <div className="absolute top-1/2 right-[10px] transform -translate-y-1/2 cursor-pointer">
             <Eye />
           </div>
         )}
-
       </div>
     </div>
   );
