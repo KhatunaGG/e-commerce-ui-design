426b6030caf44ef88f30678df175652f3d83c972
Gagloshvili <KGagloshvili@oriflame.com>
2025-06-11 15:36:22 +0400

    fixed: home-page-byRooms component

---
+++ src/app/components/__atoms/arrowRight/ArrowRight.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+function ArrowRight() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3.75 9H14.25"
+        // stroke="#377DFF"
+        stroke="#141718"
+        strokeWidth="1.125"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.75 13.5L14.25 9"
+        // stroke="#377DFF"
+        stroke="#141718"
+        strokeWidth="1.125"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.75 4.5L14.25 9"
+        // stroke="#377DFF"
+        stroke="#141718"
+        strokeWidth="1.125"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default ArrowRight;
