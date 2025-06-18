19b0b4f695546015787a6eb2431a758e6225b1a4
Gagloshvili <KGagloshvili@oriflame.com>
2025-06-18 14:39:35 +0400

    added: banner component

---
+++ src/app/components/__organism/banner/Banner.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import { ChevronRight } from "../../__atoms";
+
+const Banner = () => {
+  return (
+    <section className="w-full md:px-[11.11%] px-[8.53%]">
+      <div className="w-full  relative ">
+        <Image
+          src={"/assets/product_banner.png"}
+          alt={""}
+          width={1120}
+          height={392}
+          sizes="(max-width: 767px) 311px, 100vw"
+          className="object-cover shadow-xl w-[311px] h-[308px] md:w-full md:h-auto"
+        />
+        <div className="w-full h-full absolute inset-0 flex flex-col items-center justify-center gap-4 lg:gap-6">
+          <div className="w-fill h-fit flex  items-center justify-center pap-4">
+            <div className="flex items-center gap-1">
+              <p className="text-sm text-[#605F5F] font-medium leading-[24px]">
+                Home
+              </p>
+              <div className="w-auto h-fit flex items-center justify-center pt-1">
+                <ChevronRight />
+              </div>
+            </div>
+
+            <p className="text-sm text-[#605F5F] font-medium leading-[24px]">
+              Shop
+            </p>
+          </div>
+
+          <h1 className="font-medium text-[40px] text-[#121212] leading-[44px] md:text-[54px] md:leading-[58px] tracking-[-0.4px] md:tracking-[-1px]">
+            Shop Page
+          </h1>
+          <p className="text-base md:text-[20px] font-normal leading-[26px] md:leading-[32px] text-[#121212] px-4 md:px-0 text-center">
+            Let’s design the place you always imagined.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
--- src/app/components/__organism/nav/Nav.tsx
+++ src/app/components/__organism/nav/Nav.tsx
@@ -1,52 +1,66 @@
-// import Link from "next/link";
-// import React from "react";
-// import { Logo } from "../../__atoms";
+// "use client"
+// import { Bag, Burger, Logo, Search, User } from "../../__atoms";
+// import { NavLink } from "../../__molecules";
 
 // const Nav = () => {
 //   return (
-//     <div className=" w-full flex items-center justify-between  py-[18px]">
-//       <Logo />
-//       <div className="hidden md:flex items-center gap-10 text-sm font-medium text-[#6C7275] ">
-//         <Link href={""}>
-//           <p>Home</p>
-//         </Link>
-//         <Link href={""}>
-//           <p>Shop</p>
-//         </Link>
-//         <Link href={""}>
-//           <p>Product</p>
-//         </Link>
-//         <Link href={""}>
-//           <p>Contact Us</p>
-//         </Link>
-//       </div>
-//     </div>
+//     <>
+//       {/* <MobileMenu /> */}
+//       <section className="w-full flex items-center justify-between py-[18px]              md:px-[11.11%] px-[8.53%] ">
+//         <div className="flex items-center gap-1">
+//           <div className="w-6 h-6 flex items-center justify-center mb-[3px] md:mb-0 md:hidden">
+//             <Burger />
+//           </div>
+//           <Logo />
+//         </div>
+//         <div className="hidden md:flex items-center md:gap-6 lg:gap-10 text-sm font-medium text-[#6C7275]">
+//           <NavLink href="/">Home</NavLink>
+//           <NavLink href="/shop">Shop</NavLink>
+//           <NavLink href="/product">Product</NavLink>
+//           <NavLink href="/contact">Contact Us</NavLink>
+//         </div>
+
+//         <div className="flex items-center gap-[16px]">
+//           <Search hidden={true} />
+//           <User />
+//           <Bag />
+//           <div className="w-6 h-6 rounded-full bg-black text-white flex items-center justify-center text-xs font-bold">
+//             1
+//           </div>
+//         </div>
+//       </section>
+//     </>
 //   );
 // };
 
 // export default Nav;
 
-import React from "react";
+
+
+'use client'
+
+import { usePathname } from "next/navigation";
 import { Bag, Burger, Logo, Search, User } from "../../__atoms";
 import { NavLink } from "../../__molecules";
-// import MobileMenu from "../mobilemenu/MobileMenu";
 
 const Nav = () => {
+  const path = usePathname();
+
   return (
     <>
-      {/* <MobileMenu /> */}
-      <section className="w-full flex items-center justify-between py-[18px]              md:px-[11.11%] px-[8.53%] ">
+      <section className="w-full flex items-center justify-between py-[18px] md:px-[11.11%] px-[8.53%]">
         <div className="flex items-center gap-1">
           <div className="w-6 h-6 flex items-center justify-center mb-[3px] md:mb-0 md:hidden">
             <Burger />
           </div>
           <Logo />
         </div>
+
         <div className="hidden md:flex items-center md:gap-6 lg:gap-10 text-sm font-medium text-[#6C7275]">
-          <NavLink href="/">Home</NavLink>
-          <NavLink href="/shop">Shop</NavLink>
-          <NavLink href="/product">Product</NavLink>
-          <NavLink href="/contact">Contact Us</NavLink>
+          <NavLink href="/" isActive={path === "/"}>Home</NavLink>
+          <NavLink href="/shop" isActive={path === "/shop"}>Shop</NavLink>
+          <NavLink href="/product" isActive={path === "/product"}>Product</NavLink>
+          <NavLink href="/contact" isActive={path === "/contact"}>Contact Us</NavLink>
         </div>
 
         <div className="flex items-center gap-[16px]">
