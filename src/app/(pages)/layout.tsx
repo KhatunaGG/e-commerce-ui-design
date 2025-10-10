cfc6e8bbaf092f4aa2efee42c69823ac23ca202a
Gagloshvili <KGagloshvili@oriflame.com>
2025-10-10 18:02:50 +0400

    added: contact markup

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,26 +1,3 @@
-// import { Footer, Join, Nav, Navbar } from "../components/__organism";
-
-// export default function MainLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <section className="w-full ">
-//       <div className="w-full relative ">
-//         <Navbar />
-//         <Nav />
-//         {children}
-//       </div>
-//       <Join />
-//       <Footer />
-//     </section>
-//   );
-// }
-
-
-
-
 import { Footer, Join, Nav, Navbar } from "../components/__organism";
 
 export default function MainLayout({
--- src/app/components/__organism/saleOffer/SaleOffer.tsx
+++ src/app/components/__organism/saleOffer/SaleOffer.tsx
@@ -56,13 +56,85 @@
 
 // export default SaleOffer;
 
+// import React from "react";
+// import Image from "next/image";
+// import { ArrowRight } from "../../__atoms";
+// import Link from "next/link";
+// import { IImageData } from "@/app/store/useHomePage.store.";
+
+// export type SaleOfferProps = {
+//   images: IImageData[];
+//   page?: string;
+// };
+
+// const SaleOffer = ({ images, page }: SaleOfferProps) => {
+//   // const saleOfferPages = useMemo(() => {
+//   //   return images.filter((img) =>
+//   //     img.componentUsage?.some((c) => c.toLowerCase() === "saleoffer")
+//   //   );
+//   // }, [images]);
+
+//   if (!images.length) {
+//     return null;
+//   }
+
+//   return (
+//     <section className="w-full bg-[#F3F5F7] flex flex-col lg:flex-row jus">
+//       <div className="w-full lg:flex-1 relative min-h-[532px]">
+//         <Image
+//           src={images[0].presignedUrl}
+//           alt="Sale offer image"
+//           className="object-cover"
+//           fill
+//           priority
+//           sizes="(max-width: 1024px) 100vw, 50vw"
+//         />
+//       </div>
+//       <div
+//         className={` w-full lg:flex-1 py-[58.5px] px-8 lg:px-0 lg:py-0 lg:pl-[72px] lg:pt-[140px]`}
+
+//       >
+//         <div className="flex-1 w-auto flex flex-col items-start gap-4  text-[#377DFF]">
+//           <h3 className=" text-base font-bold leading-4 uppercase">
+//             SALE UP TO 35% OFF
+//           </h3>{" "}
+//           <h2 className=" text-[34px] font-medium leading-[38px] tracking-[-0.6px] text-[#141718] lg:text-[40px] lg:leading-11 lg:tracking-[-0.4px]">
+//             HUNDREDS of <br /> New lower prices!
+//           </h2>
+//           <p className="text-[#141718] text-[20px] font-normal leading-[32px]">
+//             It’s more affordable than ever to give every <br /> room in your
+//             home a stylish makeover
+//           </p>
+//         </div>
+
+//         <Link className="w-ful flex items-start mt-6 gap-1 " href={""}>
+//           <button className="text-base font-medium leading-[28px] tracking-[-0.4px] cursor-pointer">
+//             Shop Now
+//           </button>
+//           <div className="w-auto h-fit pt-[6px]">
+//             <ArrowRight />
+//           </div>
+//         </Link>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default SaleOffer;
+
 import React from "react";
 import Image from "next/image";
 import { ArrowRight } from "../../__atoms";
 import Link from "next/link";
 import { IImageData } from "@/app/store/useHomePage.store.";
 
-const SaleOffer = ({ images }: { images: IImageData[] }) => {
+
+export type SaleOfferProps = {
+  images: IImageData[];
+  page?: string;
+};
+
+const SaleOffer = ({ images, page }: SaleOfferProps) => {
   // const saleOfferPages = useMemo(() => {
   //   return images.filter((img) =>
   //     img.componentUsage?.some((c) => c.toLowerCase() === "saleoffer")
@@ -73,10 +145,9 @@ const SaleOffer = ({ images }: { images: IImageData[] }) => {
     return null;
   }
 
-
   return (
-    <section className="w-full bg-[#F3F5F7] flex flex-col lg:flex-row ">
-      <div className="flex-1 relative min-h-[532px]">
+    <section className="w-full bg-[#F3F5F7] flex flex-col lg:flex-row gap-[72px]">
+      <div className="w-full lg:flex-1 relative min-h-[532px]">
         <Image
           src={images[0].presignedUrl}
           alt="Sale offer image"
@@ -86,28 +157,60 @@ const SaleOffer = ({ images }: { images: IImageData[] }) => {
           sizes="(max-width: 1024px) 100vw, 50vw"
         />
       </div>
-      <div className="w-full lg:flex-1 py-[58.5px] px-8 lg:px-0 lg:py-0 lg:pl-[72px] lg:pt-[140px]">
-        <div className="flex-1 w-auto flex flex-col items-start gap-4  text-[#377DFF]">
-          <h3 className=" text-base font-bold leading-4 uppercase">
-            SALE UP TO 35% OFF
-          </h3>{" "}
-          <h2 className=" text-[34px] font-medium leading-[38px] tracking-[-0.6px] text-[#141718] lg:text-[40px] lg:leading-11 lg:tracking-[-0.4px]">
-            HUNDREDS of <br /> New lower prices!
-          </h2>
-          <p className="text-[#141718] text-[20px] font-normal leading-[32px]">
-            It’s more affordable than ever to give every <br /> room in your
-            home a stylish makeover
-          </p>
-        </div>
-
-        <Link className="w-ful flex items-start mt-6 gap-1 " href={""}>
-          <button className="text-base font-medium leading-[28px] tracking-[-0.4px] cursor-pointer">
-            Shop Now
-          </button>
-          <div className="w-auto h-fit pt-[6px]">
-            <ArrowRight />
+      <div className={` w-full lg:flex-1 flex items-center justify-center`}>
+        <div className="w-full flex flex-col md:items-start px-4 lg:px-0 pb-[63.5px] gap-6">
+          <div className="flex-1 w-auto flex flex-col items-start gap-4  text-[#377DFF]">
+            <h3
+              className={`${
+                page ? "hidden" : "flex"
+              }  text-base font-bold leading-4 uppercase`}
+            >
+              SALE UP TO 35% OFF
+            </h3>{" "}
+            <h2 className="text-[34px] font-medium leading-[38px] tracking-[-0.6px] text-[#141718] lg:text-[40px] lg:leading-11 lg:tracking-[-0.4px]">
+              {page ? (
+                <span className="block">About Us</span>
+              ) : (
+                <>
+                  <span className="block">HUNDREDS of</span>
+                  <span className="block">New lower prices!</span>
+                </>
+              )}
+            </h2>
+            <p className="text-[#141718] text-[20px] font-normal leading-[32px]">
+              {page ? (
+                <>
+                  <span className="block">
+                    3legant is a gift & decorations store based in HCMC,{" "}
+                  </span>
+                  <span className="block">Vietnam. Est since 2019.</span>
+                  <span className="block">
+                    Our customer service is always prepared to support you
+                  </span>
+                  <span className="block"> 24/7</span>
+                </>
+              ) : (
+                <>
+                  <span className="block">
+                    It’s more affordable than ever to give every
+                  </span>
+                  <span className="block">
+                    room in your home a stylish makeover{" "}
+                  </span>
+                </>
+              )}
+            </p>
           </div>
-        </Link>
+
+          <Link className="w-ful flex items-start  gap-1 " href={""}>
+            <button className="text-base font-medium leading-[28px] tracking-[-0.4px] cursor-pointer">
+              Shop Now
+            </button>
+            <div className="w-auto h-fit pt-[6px]">
+              <ArrowRight />
+            </div>
+          </Link>
+        </div>
       </div>
     </section>
   );
