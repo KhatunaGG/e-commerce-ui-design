69a1760297a1214172eb12043ba3b3568a2a1677
Gagloshvili <KGagloshvili@oriflame.com>
2025-10-20 12:10:42 +0400

    added: render-url, sign-up in MobileMenu

--- src/app/components/__organism/mobileMenu/MobileMenu.tsx
+++ src/app/components/__organism/mobileMenu/MobileMenu.tsx
@@ -1,6 +1,13 @@
 "use client";
 import Link from "next/link";
-import { Close, Search, User } from "../../__atoms";
+import {
+  Close,
+  Facebook,
+  Instagram,
+  Search,
+  User,
+  Youtube,
+} from "../../__atoms";
 import PageMenu from "../pageMenu/PageMenu";
 import { MobileMenuProps } from "@/app/interfaces/interface";
 
@@ -32,6 +39,26 @@ const MobileMenu = ({ mobileMenu, setMobileMenu }: MobileMenuProps) => {
             <User mobileMenu={mobileMenu} />
             <p>My Account</p>
           </Link>
+
+          <div className="bg-[#f1f1f1] h-[1px] w-full my-4"></div>
+
+          <div className="w-full flex flex-col gap-3 px-8 lg:px-0 bg-[#f1f1f1] rounded-xl py-2">
+            <h1 className="text-xl font-medium text-[#141718]">Sign Up</h1>
+            <p className="text-[#6C7275] text-base font-semibold">
+              Already have an account?{" "}
+              <Link
+                href="/sign-in"
+                className="text-[#38CB89] cursor-pointer inline-block transform hover:underline transition-transform duration-300 ease-in-out hover:scale-105"
+              >
+                Sign in
+              </Link>
+            </p>
+          </div>
+          <div className="w-full flex items-center justify-end gap-4 mt-4">
+            <Instagram color="black" />
+            <Facebook color="black" />
+            <Youtube color="black" />
+          </div>
         </div>
       </div>
     </div>
